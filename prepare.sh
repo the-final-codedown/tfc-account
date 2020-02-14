ca5d510a8aa5b1665aab5703e773bf05d9a74daa
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-02-14 20:14:45 +0100

    Updated prepare script

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,9 @@
 
 mvn clean package
 
-docker build -t tfc/account .
+IMAGE=tfc/account
+VERSION=
+
+docker build -t ${IMAGE} .
+docker tag ${IMAGE} localhost:5000/${IMAGE}${VERSION}
+docker push localhost:5000/${IMAGE}${VERSION}
